8baac469b5ecc294077c3c02bcb9ba6911a00434
cpp <carlospp981@gmail.com>
2025-05-22 14:35:34 +0200

    fix: Test para register para el caso de un usuario válido. Arreglado conflicto con bucle infinito

---
+++ CoWorkSpace/CoWorkSpace.Api/Migrations/20250522122932_UpdateUserFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoWorkSpace.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserFilter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
